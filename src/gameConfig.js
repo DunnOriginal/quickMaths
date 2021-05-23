05e0aaa2a0d678a8569f46d0ca78d306ae10c95a
Evan <evan.dunn96@gmial.com>
2021-05-23 02:08:38 -0300

    Update gameconfig

--- src/gameConfig.js
+++ src/gameConfig.js
@@ -9,8 +9,8 @@ export default {
   type: Phaser.CANVAS,
   width: 800,
   height: 600,
-  title: 'Phaser 3 with Parcel 📦',
-  url: 'https://github.com/samme/phaser-parcel',
+  title: 'Quick Math',
+  url: 'https://github.com/DunnOriginal/quickMaths',
   banner: { text: 'white', background: ['#FD7400', '#FFE11A', '#BEDB39', '#1F8A70', '#004358'] },
   scene: [BootScene, StoryScene, MenuScene, PlayScene, EndScene]
 };
